b80960ed2cb8006794f695f09febc4fd28271420
Saruta <fsiron@orange.fr>
2015-08-07 11:29:51 +0000

    test: add function

--- tests/break.c
+++ tests/break.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void func1 ();
+
 int main() {
   puts (" before breakpoint");
   asm volatile ("int3\n");
@@ -8,4 +10,8 @@ int main() {
   puts (" World! ");
   asm volatile ("int3\n");
   return 0;
+}
+
+void func1 () {
+  puts ("This is func1");
 }
